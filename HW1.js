f9bb5cab5eaf0b00abf12aa9c41a06293f374fa7
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-02-03 11:45:34 +0200

    Hello

---
+++ HW1.js
@@ -0,0 +1,15 @@
+let hello = "Hello"
+
+function sayHello (name) {
+    return name
+    
+}
+
+
+console.log(sayHello (hello));
+
+function nextHello () {
+    console.log("Hello")
+}
+
+nextHello ()
