0f080af74d841e14456199e85e9ff2fb4df9b895
timoxa20 <95927253+timoxa20@users.noreply.github.com>
2024-02-12 23:32:23 +0200

    ClassWork3.js

--- ClassWork3.js
+++ ClassWork3.js
@@ -1,13 +1,45 @@
-function getTimeOfDay (time) {
-    if(time<= 7) {
-        return console.log("Утро")
-    } else if( time <=12) {
-        return console.log("День")
-    } else if( time <=20) {
-        return console.log("Вечер")
-    } else  {
-        return console.log("Ночь")
+// 1)
+function getTimeOfDay(time) {
+    switch (time) {
+        case (time >= 5 || time <= 10):
+            return console.log("Утро")
+            break
+        case (time > 10 || time <= 16):
+            return console.log("День")
+            break
+        case (time > 16 || time <= 21):
+            return console.log("Вечер")
+            break
+        case (time > 21 || time < 5):
+            return console.log("Ночь")
+            break
+        default:
+            return console.log("Нет такого значения")
     }
 }
 
-getTimeOfDay (17)
+getTimeOfDay(265)
+// 2)
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+for (let i = 0; i <= 10; i++) {
+    if (i === 7) {
+        break
+    }
+    console.log(i)
+}
+
+let getNumber = 0;
+while (getNumber <= 20) {
+    getNumber++
+    if (getNumber % 2 == 0 && (getNumber / 5) % 2 !== 0) console.log(getNumber)
+}
+// 3)
+function formatPhoneNumber(str) {
+    return str = '(' + str.slice(0, 3) + ')' + ' ' + str.slice(3, 6) + '-' + str.slice(6, str.length)
+
+}
+console.log(formatPhoneNumber("1234567890"))
+
